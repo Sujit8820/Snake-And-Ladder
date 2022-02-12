9be693a346d022439acd8ec947f068a93f081d51
Sujit8820 <kadamsujit8820@gmail.com>
2022-02-12 19:17:47 +0530

    [Sujit] ADDED: The player then checks for a option.They are no play,ladder,snake

--- Snake And Ladder/Program.cs
+++ Snake And Ladder/Program.cs
@@ -3,18 +3,51 @@ using System;
 
 namespace Snake_And_Ladder
 {
-    class Programe
+    class Program
+
     {
-        public static int dieRoll()
+
+        public static int CheckOption()
+        {
+
+            Random random = new Random();
+            int option = random.Next(1, 4);
+            return option;
+        }
+        public static int DiceRoll()
         {
             Random random = new Random();
-            int dieNumber = random.Next(1,7);
-            return dieNumber;
+            int diceNumber = random.Next(1, 7);
+            return diceNumber;
+        }
+        public static int Play(int option, int diceNumber)
+        {
+            int position = 0;
+            const int LADDER = 1;
+            const int SNAKE = 2;
+            const int NOPLAY = 3;
+
+            switch (option)
+            {
+                case LADDER:
+                    position += diceNumber;
+                    break;
+
+                case SNAKE:
+                    position -= diceNumber;
+                    break;
+
+                case NOPLAY:
+
+                    break;
+
+            }
+            return position;
         }
         public static void Main(string[] args)
         {
-            int position = dieRoll();
-            Console.WriteLine("Position:" + position);
+            int place = Play(CheckOption(), DiceRoll());
+            Console.WriteLine("Position:" + place);
         }
 
     }
