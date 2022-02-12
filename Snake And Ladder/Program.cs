5ff37fa46321a898e322f9d8fae9404de0843ad2
Sujit8820 <kadamsujit8820@gmail.com>
2022-02-12 10:34:37 +0530

    [Sujit] ADDED: Game played with single player at start position 0

--- Snake And Ladder/Program.cs
+++ Snake And Ladder/Program.cs
@@ -1,2 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+namespace Snake_And_Ladder
+{
+    class Programe
+    {
+        public static void Main(string[] args)
+        {
+            int position = 0;
+            Console.WriteLine("Position:" + position);
+        }
+
+    }
+
+}
