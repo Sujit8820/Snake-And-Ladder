dfc4283469aab560424510a35085ad3f23a39784
Sujit8820 <kadamsujit8820@gmail.com>
2022-02-13 17:45:00 +0530

    [Sujit] ADDED: Play the game with 2 player

--- Snake And Ladder/Program.cs
+++ Snake And Ladder/Program.cs
@@ -6,7 +6,6 @@ namespace Snake_And_Ladder
     class Program
 
     {
-        int position = 0;
 
         public static int CheckOption()
         {
@@ -23,35 +22,79 @@ namespace Snake_And_Ladder
             // Console.WriteLine("diceno"+diceNumber);
             return diceNumber;
         }
+
+
+        public static int Turn()
+        {
+
+            Random random = new Random();
+            int turn = random.Next(1, 3);
+            return turn;
+        }
         public static void Main()
         {
+            int turn = Turn();
+            const int PLAYER_ONE = 1;
+            const int PLAYER_TWO = 2;
+
             int position = 0;
             int diceRoll = 0;
-            while (position != 100)
+            int position1 = 0;
+            int position2 = 0;
+            while (position1 != 100 && position2 != 100)
             {
                 const int LADDER = 1;
                 const int SNAKE = 2;
                 const int NOPLAY = 3;
                 int diceNumber = DiceRoll();
-                switch (CheckOption())
+                int option = CheckOption();
+                if (turn == PLAYER_ONE)
                 {
-                    case LADDER:
-                        diceRoll++;
-                        position = position + diceNumber > 100 ? position : position + diceNumber;
+                    switch (option)
+                    {
+                        case LADDER:
+                            diceRoll++;
+                            position1 = position1 + diceNumber > 100 ? position1 : position1 + diceNumber;
+                            turn = PLAYER_ONE;
+                            break;
 
-                        break;
+                        case SNAKE:
+                            diceRoll++;
+                            position1 = position1 - diceNumber < 0 ? 0 : position1 - diceNumber;
+                            turn = PLAYER_TWO;
+                            break;
 
-                    case SNAKE:
-                        diceRoll++;
-                        position = position - diceNumber < 0 ? 0 : position - diceNumber;
-                        break;
+                        case NOPLAY:
+                            turn = PLAYER_TWO;
+                            break;
 
-                    case NOPLAY:
-
-                        break;
-
+                    }
                 }
-                Console.WriteLine("Dice Roll " + diceRoll + " Position " + position);
+                else if (turn == PLAYER_TWO)
+                {
+                    switch (option)
+                    {
+                        case LADDER:
+                            diceRoll++;
+                            position2 = position2 + diceNumber > 100 ? position2 : position2 + diceNumber;
+                            turn = PLAYER_TWO;
+
+                            break;
+
+                        case SNAKE:
+                            diceRoll++;
+                            position2 = position2 - diceNumber < 0 ? 0 : position2 - diceNumber;
+                            turn = PLAYER_ONE;
+
+                            break;
+
+                        case NOPLAY:
+                            turn= PLAYER_ONE;
+                            break;
+
+                    }
+                }
+                Console.WriteLine("Dice Roll " + diceRoll + "  Player One Position " + position1 + "  Player Two Position " + position2);
             }
         }
 
